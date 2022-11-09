63141469d0562537b33c9a7b432d571aead12cd4
zhangpengbin <zeort@foxmail.com>
2022-11-09 16:57:29 +0800

    add console

--- src/demo/slider.vue
+++ src/demo/slider.vue
@@ -1,36 +1,37 @@
 <template>
-    <div id="msg">
-        <ze-slider></ze-slider>
-        <button @click="btn">点击</button>
-        <button @click="clo">取消</button>
-        <ze-button type="info" disabled>默认按钮</ze-button>
-    </div>
+  <div id="msg">
+    <ze-slider></ze-slider>
+    <button @click="btn">点击</button>
+    <button @click="clo">取消</button>
+    <ze-button type="info" disabled>默认按钮</ze-button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "HelloWorld",
-    props: {
-        msg: String,
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      num: 0,
+    };
+  },
+  mounted() {},
+  methods: {
+    btn() {
+      console.log(1);
+      console.log(4444444444);
+      this.$message.info("这是一条提示语句");
     },
-    data() {
-        return {
-            num: 0,
-        };
+    clo() {
+      console.log(4444);
+      this.$message.remove();
     },
-    mounted() {},
-    methods: {
-        btn() {
-            this.$message.info('这是一条提示语句');
-        },
-        clo() {
-            console.log(4444)
-            this.$message.remove()
-        }
-    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
